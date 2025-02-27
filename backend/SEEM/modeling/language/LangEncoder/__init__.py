1ff4a67c7cb91a67098c7ee2272b03bf8535efe5
Pureblackkk <luoyunzhong7@gmail.com>
2025-02-27 17:44:18 +0800

    feat: re-run all images panoptic

--- backend/SEEM/modeling/language/LangEncoder/__init__.py
+++ backend/SEEM/modeling/language/LangEncoder/__init__.py
@@ -18,13 +18,13 @@ def build_tokenizer(config_encoder):
     os.environ['TOKENIZERS_PARALLELISM'] = 'true'
     if config_encoder['TOKENIZER'] == 'clip':
         pretrained_tokenizer = config_encoder.get(
-            'PRETRAINED_TOKENIZER', 'openai/clip-vit-base-patch32'
+            'PRETRAINED_TOKENIZER', '/home/pureblackkkk/my_volume/models/clip-vit-base-patch32'
         )
         tokenizer = CLIPTokenizer.from_pretrained(pretrained_tokenizer)
         tokenizer.add_special_tokens({'cls_token': tokenizer.eos_token})
     elif config_encoder['TOKENIZER'] == 'clip-fast':
         pretrained_tokenizer = config_encoder.get(
-            'PRETRAINED_TOKENIZER', 'openai/clip-vit-base-patch32'
+            'PRETRAINED_TOKENIZER', '/home/pureblackkkk/my_volume/models/clip-vit-base-patch32'
         )
         tokenizer = CLIPTokenizerFast.from_pretrained(pretrained_tokenizer, from_slow=True)
     else:
